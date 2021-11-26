978b40913c0352f8021afa584839eea31d01d4de
jonasmcarvalho <jonasmcarvalho@outlook.com>
2021-11-26 07:14:04 -0300

    jsonwebtoken - criado token de autenticação do usuario

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,59 @@
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+import { inject, injectable } from 'tsyringe';
+
+import { IUsersRepository } from '../../repositories/IUsersRepository';
+
+interface IRquest {
+    email: string;
+    password: string;
+}
+
+interface IResponse {
+    user: {
+        name: string;
+        email: string;
+    };
+    token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+    constructor(
+        @inject('UsersRepository')
+        private usersRepository: IUsersRepository,
+    ) { }
+
+    async execute({ email, password }: IRquest): Promise<IResponse> {
+        // Usuario existe?
+        const user = await this.usersRepository.findByEmail(email);
+
+        if (!user) {
+            throw new Error('Email or password incorrect');
+        }
+        const passwordMatch = await compare(password, user.password);
+
+        // senha está correta
+        if (!passwordMatch) {
+            throw new Error('Email or password incorrect!');
+        }
+
+        // gerar jsonwebtoken
+        const token = sign({}, '0890bc70c5941bf41eaad391c303f9b2', {
+            subject: user.id,
+            expiresIn: '1d',
+        });
+
+        const tokenReturn: IResponse = {
+            token,
+            user: {
+                name: user.name,
+                email: user.email,
+            },
+        };
+
+        return tokenReturn;
+    }
+}
+
+export { AuthenticateUserUseCase };
