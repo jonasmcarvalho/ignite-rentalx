f7a0d887db67909f2d38ec8bc9621e20e3b2a1c4
jonasmcarvalho <jonasmcarvalho@outlook.com>
2021-12-04 08:48:14 -0300

    criado upload de imagens
    Criado useCase para upload de multiplas imagens

--- README.md
+++ README.md
@@ -37,7 +37,6 @@ O usuário responsavel pelo cadastro deve ser um usuário admistrador.
 
 **RF**
 Deve ser possível cadastrar a imagem do carro.
-Deve ser possível listar todos os carros
 
 **RNF**
 Utilizar o multer para upload dos arquivos
--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
+import multer from 'multer';
 
+import uploadConfig from '@config/upload';
 import { CreateCarController } from '@modules/cars/useCases/createCar/CreateCarController';
 import { CreateCarSpecificationController } from '@modules/cars/useCases/createCarSpecification/CreateCarSpecificationController';
 import { ListAvailableCarsController } from '@modules/cars/useCases/listAvailableCars/ListAvailableCarsController';
-import { ListAvailableCarsUseCase } from '@modules/cars/useCases/listAvailableCars/ListAvailableCarsUseCase';
+import { UploadCarImagesController } from '@modules/cars/useCases/uploadCarImages/UploadCarImagesController';
 
 import { ensureAdmin } from '../middlewares/ensureAdmin';
 import { ensureAuthentidated } from '../middlewares/ensureAuthenticated';
@@ -13,6 +15,9 @@ const carsRoutes = Router();
 const createCarController = new CreateCarController();
 const listAvailableCarsController = new ListAvailableCarsController();
 const createCarSpecificationController = new CreateCarSpecificationController();
+const uploadCarImagesController = new UploadCarImagesController();
+
+const upload = multer(uploadConfig.upload('./tmp/cars'));
 
 carsRoutes.post(
     '/',
@@ -28,6 +33,14 @@ carsRoutes.post(
     ensureAuthentidated,
     ensureAdmin,
     createCarSpecificationController.handle,
+);
+
+carsRoutes.post(
+    '/images/:id',
+    ensureAuthentidated,
+    ensureAdmin,
+    upload.array('images'),
+    uploadCarImagesController.handle,
 );
 
 export { carsRoutes };
