a2b1a12df424022ab292ba7dbaad7511afd30d2e
jonasmcarvalho <jonasmcarvalho@outlook.com>
2021-12-04 21:51:33 -0300

    criando aluguel de carro
    Criado migration de aluguel e aplicado testes

--- src/shared/infra/typeorm/migrations/1637744997885-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1637744997885-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1637744997885 implements MigrationInterface {
                     {
                         name: 'id',
                         type: 'uuid',
+                        isPrimary: true,
                     },
                     {
                         name: 'name',
