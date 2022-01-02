78749d6e24b4a17d7fb3b8efe0060082bbc3c217
jonasmcarvalho <jonasmcarvalho@outlook.com>
2022-01-02 08:22:08 -0300

    Criado URL de visualização
    Criado URL de visualização de avatar e carros

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -3,6 +3,7 @@ import multer from 'multer';
 
 import uploadConfig from '@config/upload';
 import { CreateUserController } from '@modules/accounts/useCases/createUser/CreateUserController';
+import { ProfileUserController } from '@modules/accounts/useCases/profileUserUseCase/profileUserController';
 import { UpdateUserAvatarController } from '@modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController';
 import { ensureAuthenticated } from '@shared/infra/http/middlewares/ensureAuthenticated';
 
@@ -12,8 +13,11 @@ const uploadAvatar = multer(uploadConfig);
 
 const createUserController = new CreateUserController();
 const updateAvatarController = new UpdateUserAvatarController();
+const profileUserController = new ProfileUserController();
 
 usersRoutes.post('/', createUserController.handle);
+
+usersRoutes.get('/', ensureAuthenticated, profileUserController.handle);
 
 usersRoutes.patch(
     '/avatar',
